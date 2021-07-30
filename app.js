e10048745e16412e015d629169b570240064b01d
duc <hmduc227@gmail.com>
2021-07-30 21:53:12 +0700

    link database

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Back end is runing bois"));
@@ -8,6 +10,22 @@ app.get("/user/:username", (req, res) => {
   res.send({ username: req.params.username, password: "admin" });
   console.log("finish");
 });
+
+let connecttion = mysql.createConnection({
+  host: "us-cdbr-east-04.cleardb.com",
+  user: "bc16d12ee7c8eb",
+  password: "a8e818c7",
+});
+
+connecttion.connect(function (err) {
+  if (err) throw err;
+  console.log("DB connected!");
+});
+
+// mysql.connect(
+//   "mysql://:@/heroku_bafd04ba4f49282?reconnect=true",
+//   () => console.log("connected DB")
+// );
 
 app.listen(process.env.PORT || port, () =>
   console.log("Example app listening on port %s!", port)
