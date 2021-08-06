566ae88fe604be826571389c0a6cc121cc06dc67
duc <hmduc227@gmail.com>
2021-08-06 22:17:04 +0700

    finish delete user

--- routes/users.js
+++ routes/users.js
@@ -4,27 +4,50 @@ import mysql from 'mysql';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const result = await userController.getUser();
-  res.json(result);
+  try {
+    const result = await userController.getUser();
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 router.post('/', async (req, res) => {
   try {
     const result = await userController.postUser(req.body);
     res.json(result);
-  } catch (err) {
-    res.json(err);
+  } catch (error) {
+    res.json(error);
   }
 });
 
 router.get('/:username', async (req, res) => {
-  const result = await userController.getUserByUsername(req.params.username);
-  res.json(result);
+  try {
+    const result = await userController.getUserByUsername(req.params.username);
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 router.patch('/:username', async (req, res) => {
-  const result = await userController.patchUserByUsername(req.body);
-  res.json(result);
+  try {
+    const result = await userController.patchUserByUsername(req.body);
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.delete('/:username', async (req, res) => {
+  try {
+    const result = await userController.deleteUserByUsername(
+      req.params.username
+    );
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 export { router };
